4917b7e019fdb326e030e9c6943842a582a75ae3
Oren Heimlich <oren.he@d-bank.co.il>
2021-02-28 15:04:36 +0200

    Setup appcenter pre build file.

--- ExternalDemo/appcenter-pre-build.sh
+++ ExternalDemo/appcenter-pre-build.sh
@@ -12,5 +12,5 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" != "main" ];
 then
-    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/MyApp/Info.plist
+    plutil -replace CFBundleDisplayName -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/comments-Info/Info.plist
 fi
