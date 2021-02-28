db76186b06ef251a2b8284c10980bc04b3fb6c20
Oren Heimlich <oren.he@d-bank.co.il>
2021-02-28 13:26:29 +0200

    Add shell file.

---
+++ BasicDemo/NormalExample/NormalExample/ZJTableViewManager/appcenter-pre-build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  appcenter-pre-build.sh
+#  discount
+#
+#  Created by Oren Heimlich on 25/02/2021.
+#  Copyright © 2021 Discount. All rights reserved.
+
+#!/usr/bin/env bash
+
+# Example: Clone a required repository
+git clone https://github.com/orendbank/ZJTableViewManager.git
+
+# Example: Install App Center CLI
+npm install -g appcenter-cli
